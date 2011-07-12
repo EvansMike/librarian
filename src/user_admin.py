ebc25e183619f21c86407fa838699129af6b99aa
mikee@millstream-computing.co.uk <mikee@millstream-computing.co.uk@4a23c50b-7d38-45fb-897e-ce209cb83afc>
2011-07-12 11:58:30 +0000

    Add framework.  Does nothing as yet.
    git-svn-id: https://simple-librarian.googlecode.com/svn@21 4a23c50b-7d38-45fb-897e-ce209cb83afc

--- src/user_admin.py
+++ src/user_admin.py
@@ -1,20 +1,50 @@
 #!/bin/env python
+'''
+       user_admin.py
 
-#       user_admin.py
-#
-#       Copyright 2011 Mike Evans <mikee@millstreamcomputing.co.uk>
-#
-#       This program is free software; you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
-#       (at your option) any later version.
-#
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#
-#       You should have received a copy of the GNU General Public License
-#       along with this program; if not, write to the Free Software
-#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#       MA 02110-1301, USA.
+       Copyright 2011 Mike Evans <mikee@millstreamcomputing.co.uk>
+
+       This program is free software; you can redistribute it and/or modify
+       it under the terms of the GNU General Public License as published by
+       the Free Software Foundation; either version 2 of the License, or
+       (at your option) any later version.
+
+       This program is distributed in the hope that it will be useful,
+       but WITHOUT ANY WARRANTY; without even the implied warranty of
+       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+       GNU General Public License for more details.
+
+       You should have received a copy of the GNU General Public License
+       along with this program; if not, write to the Free Software
+       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+       MA 02110-1301, USA.
+'''
+'''
+Add new borrowers to the list.  Remove too I guess.
+'''
+import MySQLdb
+import MySQLdb.cursors
+import sys
+import load_config
+import logging
+import book
+import locale
+import gettext
+import popen2
+
+locale.setlocale(locale.LC_ALL, '')
+APP = 'librarian'
+gettext.textdomain(APP)
+_ = gettext.gettext
+
+logger = logging.getLogger("librarian")
+logging.basicConfig(format='%(module)s: LINE %(lineno)d: %(levelname)s:%(message)s', level=logging.DEBUG)
+
+# Read the config file
+
+config = load_config.load_config()
+db_user = config.db_user
+db_pass = config.db_pass
+db_base = config.db_base
+db_host = config.db_host
+
