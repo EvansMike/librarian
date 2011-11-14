bb971d856e596eae64a757b947aa174281e82efc
Mike Evans <mikee@millstreamcomputing.co.uk>
2011-11-14 14:54:13 +0000

    Changed to use database directly.

--- src/import_calibre.py
+++ src/import_calibre.py
@@ -3,20 +3,71 @@
 Import e-books from calibre database avoiding duplicates
 Run with calibre-debug -e import_calibre.py
 '''
-import sys
-import site
+import sys, os
+import sqlite3
+import load_config
+import logging
+import locale
+import gettext
 
-#sys.path.append("/opt/calibre/lib/python2.7/site-packages")
-#site.USER_SITE="/opt/calibre/lib/python2.7/site-packages"
+plat = sys.platform
 
-import calibre
-import calibre.library
+#Platform dependent stuff
+if plat == "darwin":
+  pass # plat = "mac"
+elif plat== "win32":
+  pass #plat = "windows"
+elif plat == "linux2":
+  HOME_DIR = os.getenv('HOME')
+  pass #linuxStuff()
 
-calibre.library.db("")
-base = calibre.library.database.LibraryDatabase("/home/mikee/Calibre_Library/metadata.db")
-authors = base.all_authors()
-titles = base.all_titles()
-author_title = dict(zip(authors, titles))
+locale.setlocale(locale.LC_ALL, '')
+APP = 'librarian'
+gettext.textdomain(APP)
+_ = gettext.gettext
 
-for t,a in  map(None,titles, authors):
-  print t,a
+logger = logging.getLogger("librarian")
+logging.basicConfig(format='%(module)s: LINE %(lineno)d: %(levelname)s:%(message)s', level=logging.DEBUG)
+
+
+class calibre_import:
+  ''' Do the import and insert the data into the list.
+  Called from librarian.py
+
+  '''
+  def __init__(self):
+    # Read the config file
+    config = load_config.load_config()
+    self.calibre_base = HOME_DIR + "/" + config.calibre_db
+    logging.info(self.calibre_base)
+    pass
+
+  def read_db(self):
+
+    pass
+
+  def insert_data(self, booklist):
+    '''Insert the data into a booklist and return the list.
+
+    Parameters:
+    booklist -- a GTKListStore object.
+
+    '''
+
+    conn = sqlite3.connect(self.calibre_base)
+    c = conn.cursor()
+    ''' Example safe query
+    t = (symbol,)
+    c.execute('select * from books where symbol=?', t)
+    '''
+    '''
+    c.execute('select name, title from authors, books, books_authors_link \
+    where books_authors_link.book=books_authors_link.author;')
+    '''
+    for row in c:
+      print row
+
+
+if __name__ == "__main__":
+  app = calibre_import()
+  app.insert_data(None)
