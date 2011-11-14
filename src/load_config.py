607a9578b7e9fcf7189d135b3be5934d0869c69b
Mike Evans <mikee@millstreamcomputing.co.uk>
2011-11-14 14:50:19 +0000

    We don't write a file because it has a missing field, only if the file is missing.

--- src/load_config.py
+++ src/load_config.py
@@ -29,21 +29,16 @@ class load_config:
     config = ConfigParser.ConfigParser()
     try:
       config.read(config_file)
-      self.db_user = config.get('database','USER')
-      self.db_pass = config.get('database','PASSWD')
-      self.db_base = config.get('database','DB')
-      self.db_host = config.get('database','DBHOST')
-      self.calibre_db = config.get('calibre','CALIBRE_DB')
     except:
       print "Cannot read config file, exiting.\n"
       # Print a blank config file and inform the luser
       d = gtk.Dialog()
       d.add_buttons(gtk.STOCK_OK, 1)
 
+      # Pop up a message
       label = gtk.Label(_('No config file found. Please edit ') + config_file)
       label.show()
       d.vbox.pack_start(label)
-
       answer = d.run()
       d.destroy()
 
@@ -52,17 +47,16 @@ class load_config:
       f.write('[database]\nUSER = username\nPASSWD = password\nDB = db_name\nDBHOST = hostname\n\
       \n# Define relative path to Calibre database, Users home dir will be\
       automatically determined.\n[calibre]\nCALIBRE_DB\n')
-      '''
-      [database]
-      USER =
-      PASSWD =
-      DB =
-      DBHOST =
-      '''
-      #f.close()
       os.fchmod(f.fileno(),stat.S_IREAD|stat.S_IWRITE)
       f.close()
       del f
+
+    # Now read the file
+    self.db_user = config.get('database','USER')
+    self.db_pass = config.get('database','PASSWD')
+    self.db_base = config.get('database','DB')
+    self.db_host = config.get('database','DBHOST')
+    self.calibre_db = config.get('calibre','CALIBRE_DB')
 
 
 
