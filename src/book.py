63bcf44fe3b3fa2c025ba0ce72e02008f77125fd
Mike Evans <mikee@millstreamcomputing.co.uk>
2011-11-15 13:27:18 +0000

    Add GPL.

--- src/book.py
+++ src/book.py
@@ -1,4 +1,23 @@
 #!/bin/env python
+'''
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+  MA 02110-1301, USA.
+
+A (in)complete home book collection manager.
+Could do with extending to cover e-books, CDs and DVDs perhaps?
+'''
 # Define a book object
 # TODO: No longer sure about storing the number oc copies of a book as
 # that would prevent me from storing the scanned (or when I bought) the
