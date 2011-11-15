5fd6d1acc21ad514216d94d58c12af77fc1ebf4c
Mike Evans <mikee@millstreamcomputing.co.uk>
2011-11-15 13:37:32 +0000

    Fix GPL.

--- src/load_config.py
+++ src/load_config.py
@@ -15,9 +15,6 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
   MA 02110-1301, USA.
-
-A (in)complete home book collection manager.
-Could do with extending to cover e-books, CDs and DVDs perhaps?
 '''
 
 import sys
