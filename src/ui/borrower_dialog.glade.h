323aad124085e92d6236761a73e7ff28765b050f
Mike Evans <mikee@millstreamcomputing.co.uk>
2012-07-14 13:54:00 +0100

    New.

---
+++ src/ui/borrower_dialog.glade.h
@@ -0,0 +1,7 @@
+char *s = N_("CANCEL");
+char *s = N_("Contact: ");
+char *s = N_("Name: ");
+char *s = N_("Notes: ");
+char *s = N_("PRINT BORROWERS");
+char *s = N_("SAVE");
+char *s = N_("label");
