e0b7c8c698d3a18704b1d8aaf9b52b829516ad12
Mike Evans <mikee@millstreamcomputing.co.uk>
2012-07-15 16:39:02 +0100

    Version

--- src/librarian.py
+++ src/librarian.py
@@ -52,7 +52,7 @@ logging.basicConfig(format='%(module)s: LINE %(lineno)d: %(levelname)s:%(message
 plat = sys.platform
 
 
-__version__ = "20120715-218"
+__version__ = "20120715-220"
 
 try:
   import pygtk
