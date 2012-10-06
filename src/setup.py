f03b5a378cdef6c587d730153b445d2125eb6b85
Mike Evans <mikee@millstreamcomputing.co.uk>
2012-10-07 00:05:44 +0100

    Update primitive setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 ### setup.py ###
-#from distutils.core import setup
+from distutils.core import setup
 import os
-from setuptools import setup
+#from setuptools import setup
 import version
+# We create the package with
+# python setup.py sdist 
 
-def read(fname):
-  return open(os.path.join(os.path.dirname(__file__), fname)).read()
   
 setup (name='Librarian',
       version=version.__version__,
@@ -15,9 +15,15 @@ setup (name='Librarian',
       author_email='mikee@saxicola.co.uk',
       url='https://github.com/EvansMike/librarian.git',
       license='GNU General Public License',
-      py_modules=['librarian'],
-      long_description=read('../README'),
+      py_modules=['add_edit'\
+      'admin','amazonlookup','book','borrowers','calibre','create_qr',\
+      'db_queries','extract_books','freebase_lookup','freebase-lookup-sample',\
+      'gconf_config','get_orders','guiscan','lib_print','librarian',\
+      'load_config','lookup_books','media_admin','messages','setup',\
+      'user_admin','version',
+      ],
       #packages=['librarian'],
-      package_data={'librarian': ['po/*']}
+      package_data={'librarian': ['po/*']
+      }
 )
 
