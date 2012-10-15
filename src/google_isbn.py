66e0958cfcf4e10c49e7c3a9d9a27733ce9ebfe1
Mike Evans <mikee@millstreamcomputing.co.uk>
2012-10-15 21:30:07 +0100

    Add a sample google books API lookup.

---
+++ src/google_isbn.py
@@ -0,0 +1,30 @@
+#!/bin/env python
+'''
+From:
+http://lateral.netmanagers.com.ar/weblog/posts/quick-hack-to-catalog-your-books.html
+'''
+
+import sys
+import time
+import gdata.books.service
+import json
+
+def get_book_info(isbn):
+    print "Looking for ISBN:", isbn
+    google_books = gdata.books.service.BookService()
+    result = google_books.search('ISBN %s '%isbn)
+    data = [x.to_dict() for x in result.entry]
+    if not data:
+        print "No results"
+        return
+    title = data[0]['title']
+    with open(title+'.json','w') as f:
+        f.write(json.dumps(data))
+    print "Guardada info de '%s' en '%s.json'" %(isbn, title)
+
+if __name__ == "__main__":
+    while True:
+        isbn = sys.stdin.readline().strip()
+        if isbn:
+            get_book_info(isbn)
+        time.sleep(1)
