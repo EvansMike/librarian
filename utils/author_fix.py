d28125b89d1a13e348b2f754f702206fb0bc79ad
Mike Evans <mikee@millstreamcomputing.co.uk>
2013-08-02 11:37:54 +0100

    Re-indent

--- utils/author_fix.py
+++ utils/author_fix.py
@@ -30,79 +30,79 @@ db_pass = config.db_pass
 db_base = config.db_base
 db_host = config.db_host
 
-      
+
 def parse_isbn_books():
-  '''
-  Attempt to parse the current file.
-  '''
-  db = MySQLdb.connect(host=db_host, db=db_base,  passwd = db_pass)
-  cur = db.cursor(MySQLdb.cursors.DictCursor)
-  cur.execute("DELETE FROM books_to_authors") # Clean the tables
-  cur.execute("DELETE FROM book_authors") # Clean the tables
-  cur.execute("DELETE FROM new_books") # Clean the tables
-  
-  
-  command = "SELECT id, isbn FROM books where isbn !='' ORDER BY id;" # LIMIT for testing
-  numrows = cur.execute(command)
-  books = cur.fetchall()
-  complex_names = []
-  for mybook in books:
-    
-    abook = book.book()
-    abook.webquery(mybook['isbn'])
-    
-    authors = abook.authors
-    print "" ## Debug
-    print abook.__dict__ ## Debug
-    cur.execute("INSERT INTO new_books(title, author, isbn,abstract, \
-      year, publisher, city, copies, mtype, add_date, owner) \
-      VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", \
-      (abook.title, abook.authors, abook.isbn, abook.abstract, \
-      abook.year, abook.publisher, abook.city, 1, abook.mtype, abook.add_date, abook.owner))
-    db.commit()
-    cur.execute("SELECT LAST_INSERT_ID()")
-    last_book_id = cur.fetchone()
-    last_book_id = last_book_id['LAST_INSERT_ID()']  
-    #print "last_book_id = ",last_book_id
-    split_suthors = authors.split(",")
-    ordinal = 0
-    print abook.title, abook.authors
-    for author in split_suthors:
-      if author == '' : continue
-      last = author.split()[-1]
-      first = author.split()[0]
-      #print "Ordinal = ", ordinal, author, first, last
-      cur.execute("INSERT IGNORE INTO book_authors(author_last, author_first) \
-                  VALUES(%s, %s)", (last, first))
-      #last_author_id = cur.execute("SELECT author_id FROM book_authors ORDER BY author_id DESC LIMIT 0 , 1")
-      db.commit()
-      cur.execute("SELECT LAST_INSERT_ID()")
-      last_author_id = cur.fetchone()
-      last_author_id = last_author_id['LAST_INSERT_ID()']
-      
-      cur.execute("INSERT INTO books_to_authors(book_id, author_id, author_ordinal) \
-          VALUES(%s,%s,%s)", (last_book_id,last_author_id,  ordinal))
-      db.commit()
-      ordinal += 1
+    '''
+    Attempt to parse the current file.
+    '''
+    db = MySQLdb.connect(host=db_host, db=db_base,  passwd = db_pass)
+    cur = db.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("DELETE FROM books_to_authors") # Clean the tables
+    cur.execute("DELETE FROM book_authors") # Clean the tables
+    cur.execute("DELETE FROM new_books") # Clean the tables
+
+
+    command = "SELECT id, isbn FROM books where isbn !='' ORDER BY id;" # LIMIT for testing
+    numrows = cur.execute(command)
+    books = cur.fetchall()
+    complex_names = []
+    for mybook in books:
+
+        abook = book.book()
+        abook.webquery(mybook['isbn'])
+
+        authors = abook.authors
+        print "" ## Debug
+        print abook.__dict__ ## Debug
+        cur.execute("INSERT INTO new_books(title, author, isbn,abstract, \
+          year, publisher, city, copies, mtype, add_date, owner) \
+          VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", \
+          (abook.title, abook.authors, abook.isbn, abook.abstract, \
+          abook.year, abook.publisher, abook.city, 1, abook.mtype, abook.add_date, abook.owner))
+        db.commit()
+        cur.execute("SELECT LAST_INSERT_ID()")
+        last_book_id = cur.fetchone()
+        last_book_id = last_book_id['LAST_INSERT_ID()']
+        #print "last_book_id = ",last_book_id
+        split_suthors = authors.split(",")
+        ordinal = 0
+        print abook.title, abook.authors
+        for author in split_suthors:
+            if author == '' : continue
+            last = author.split()[-1]
+            first = author.split()[0]
+            #print "Ordinal = ", ordinal, author, first, last
+            cur.execute("INSERT IGNORE INTO book_authors(author_last, author_first) \
+                        VALUES(%s, %s)", (last, first))
+            #last_author_id = cur.execute("SELECT author_id FROM book_authors ORDER BY author_id DESC LIMIT 0 , 1")
+            db.commit()
+            cur.execute("SELECT LAST_INSERT_ID()")
+            last_author_id = cur.fetchone()
+            last_author_id = last_author_id['LAST_INSERT_ID()']
+
+            cur.execute("INSERT INTO books_to_authors(book_id, author_id, author_ordinal) \
+                VALUES(%s,%s,%s)", (last_book_id,last_author_id,  ordinal))
+            db.commit()
+            ordinal += 1
 
 def parse_non_isbn_books():
-  db = MySQLdb.connect(host=db_host, db=db_base,  passwd = db_pass)
-  cur = db.cursor(MySQLdb.cursors.DictCursor)
-  # Now the books with no ISBNs
-  cur.execute("SELECT * FROM books where isbn ='' ORDER BY id;")
-  # etc.
-  books = cur.fetchall()
-  for abook in books:
-    print abook['title'] 
-    
-    
-  return;
+    db = MySQLdb.connect(host=db_host, db=db_base,  passwd = db_pass)
+    cur = db.cursor(MySQLdb.cursors.DictCursor)
+    # Now the books with no ISBNs
+    cur.execute("SELECT * FROM books where isbn ='' ORDER BY id;")
+    # etc.
+    books = cur.fetchall()
+    for abook in books:
+        print abook['title']
+
+
+    return;
 
 
 
 
 ''' Run main if called directly.'''
 if __name__ == "__main__":
-  parse_non_isbn_books() 
-  #app = librarian()
-  #gtk.main()
+    parse_non_isbn_books()
+    #app = librarian()
+    #gtk.main()
