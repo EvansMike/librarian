83dd5ea17abbb76555597cf02baedbfc56c67978
Mike Evans <mikee@saxicola.co.uk>
2015-07-11 15:56:22 +0100

    Use response not urllib2, also other stuff.

--- src/lookup_books.py
+++ src/lookup_books.py
@@ -1,35 +1,51 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
-
-import json
-import urllib
-import urllib2
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(module)s: LINE %(lineno)d: %(levelname)s: %(message)s')
-DEBUG = logging.debug
+import requests
+#import logging
+#logging.basicConfig(level=logging.DEBUG, format='%(module)s: LINE %(lineno)d: %(levelname)s: %(message)s')
+#DEBUG = logging.debug
 
 
 class BookLookup(object):
 
+    def retry(func):
+        ''' decorator function to retry'''
+        
+        def retried_func(*args, **kwargs):
+            MAX_TRIES = 3
+            tries = 0
+            while True:
+                resp = func(*args, **kwargs)
+                if resp.status_code != 200 and tries < MAX_TRIES:
+                    tries += 1
+                    continue
+                break
+            return resp
+        return retried_func
 
     def google_desc(self, isbn):
         url = "https://www.googleapis.com/books/v1/volumes?q=isbn+{isbn}"\
           "&fields=items/volumeInfo(description)"\
           "&maxResults=1".format(isbn=isbn)
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        content = json.loads(response.read())
-        DEBUG(content)
+        r = None
+        while True:
+            r = requests.get(url)
+            if r.status_code == 200:
+                break
+        content = r.json()
         return content['items'][0]['volumeInfo']['description'].encode('utf8')
-
+    
+    #@retry
     def xisbn(self, isbn):
         url = 'http://xisbn.worldcat.org/webservices/xid/isbn/{isbn}?'\
         'method=getMetadata&format=json&fl=title,author,year,publisher,lang,city'.format(isbn=isbn)
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        content = json.loads(response.read())
-        DEBUG(content)
+        r = None
+        while True:
+            r = requests.get(url)
+            if r.status_code == 200:
+                break
+        content = r.json()
         content = content['list'][0]
         book = {}
         book['isbn'] = isbn
@@ -50,10 +66,6 @@ class BookLookup(object):
 if __name__ == '__main__':
     lookup = BookLookup()
     data = lookup.xisbn("0130104949")
-    descript = lookup.google_desc("0130104949")
     print data
-    print descript
     data = lookup.xisbn("1565924339")
-    descript = lookup.google_desc("1565924339")
     print data
-    print descript
